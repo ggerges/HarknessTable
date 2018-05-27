c224138dc5a4c12ba9bc887e73d09964e14107e6
Caitlyn Mei <cmei@bayviewglen.ca>
2018-05-27 14:19:44 -0400

    ...

--- starFruit/client/src/components/HarknessTable.vue
+++ starFruit/client/src/components/HarknessTable.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="hello">
     <h1>Harkness Table</h1>
+    <br>
+    <canvas id="myCanvas" width="800" height="500"></canvas>
+    <!--
     <drag class="drag"
       :image="require('../assets/starfruit.jpeg')"
       :transfer-data="{ example: 'drag-image' }">
-      drag to HERE
+      drag from HERE
     </drag>
     <br><br>
-    <drop class="drop" @drop="handleDrop">drop</drop>
+    <drop class="drop" @drop="handleDrop">drop to here</drop>
     <br><br>
+    -->
+    <br><br><br><br>
   </div>
 </template>
 
 <script>
+/*
 import { Drag, Drop } from 'vue-drag-drop'
 export default {
   name: 'HarknessTable',
@@ -22,8 +28,11 @@ export default {
       alert('Help me...')
     }
   }
-}
+}*/
 </script>
 
 <style type="text/css">
+canvas {
+  border-style: solid;
+}
 </style>
