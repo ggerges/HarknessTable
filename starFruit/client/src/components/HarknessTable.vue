f83066ee80cb98e6b6ec1418e19b84ae942ded7e
Caitlyn Mei <cmei@bayviewglen.ca>
2018-05-27 13:31:27 -0400

    fixed bug

--- starFruit/client/src/components/HarknessTable.vue
+++ starFruit/client/src/components/HarknessTable.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="hello">
     <h1>Harkness Table</h1>
-    <img src="../assets/starfruit.jpeg">
     <drag class="drag"
       :image="require('../assets/starfruit.jpeg')"
       :transfer-data="{ example: 'drag-image' }">
-      drag
+      drag to HERE
     </drag>
+    <br><br>
     <drop class="drop" @drop="handleDrop">drop</drop>
     <br><br>
   </div>
 </template>
 
 <script>
-import { Drag, Drop } from 'vue-drag-drop';
+import { Drag, Drop } from 'vue-drag-drop'
 export default {
   name: 'HarknessTable',
   components: { Drag, Drop },
   methods: {
-    handleDrop(data) {
-      alert('You dropped with data: ${JSON.stringify(data)}')
+    handleDrop (data) {
+      alert('Help me...')
     }
   }
 }
