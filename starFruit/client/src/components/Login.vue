eff13419a3e26e0d9f7db8310a8d8c55cac2c7fd
Gaby <ggerges@bayviewglen.ca>
2018-05-29 13:52:22 -0400

    taking a break from dis stupid stuff

--- starFruit/client/src/components/Login.vue
+++ starFruit/client/src/components/Login.vue
@@ -1,7 +1,18 @@
 <template>
 
-  <div class="hello">
-    <h1>LOGIN</h1>
+  <div class="login">
+     <div class="wrapper">
+    <form class="form-signin">
+      <h2 class="form-signin-heading">Please login</h2>
+      <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+      <label class="checkbox">
+      <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
+      </label>
+      <button class="btn btn-lg btn-primary btn-block" v-on:click.native ="greet" >Login</button>
+    </form>
+  </div>
+  <!--  <h1>LOGIN</h1>
     <br>
     <div id = "greyBox"></div>
    <div id = "items">
@@ -31,46 +42,71 @@
       </li>
     </ul>
   </div>
+-->
 </div>
 </template>
 
 <script>
-
-export default {
-  name: 'Login',
-  data () {
-    return {
-      msg: 'Welcome to Your Harkness Table App'
+export default{
+  data: {
+      form: {
+        email: '',
+        password: '',
+      }
     }
   }
-}
+
 </script>
 <!-- script type "text/javascript" src="client/jquery/jquery.js"></script>-->
-
 <!-- Add "scoped" attribute to limit CSS to this component only (scoped for child css) -->
 <style scoped>
-#items{
-margin-top: -38%;
+  body {
+  background: #eee !important;
 }
-#greyBox{
-  width: 300px;
-  height: 500px;
-  background-color: #A5A3A3;
-  border-radius: 20px;
-  margin:3%;
+.wrapper {
+  margin-top: 80px;
+  margin-bottom: 80px;
 }
-h1{
-  font-weight: normal;
+
+.form-signin {
+  max-width: 380px;
+  padding: 15px 35px 45px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.1);
+
+  .form-signin-heading,
+  .checkbox {
+    margin-bottom: 30px;
+  }
+
+  .checkbox {
+    font-weight: normal;
+  }
+
+  .form-control {
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    @include box-sizing(border-box);
+
+    &:focus {
+      z-index: 2;
+    }
+  }
+
+  input[type="text"] {
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  input[type="password"] {
+    margin-bottom: 20px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
